e4be15ba2426e83a66a8d8b722a511476ff0c018
Angel <angelchen1929@gmail.com>
2024-03-06 13:30:26 -0500

    update1

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,11 +1,13 @@
 import React from 'react';
-import fatbunny from './fatbunny.jpeg';;
+import fatbunny from './fatbunny.jpeg';
+import navbar from './NavBar/index.js';
 
 console.log(fatbunny);
 
 const HomePage = () => {
     
     return (
+        
         <div>
             <center>
                 <header> 
--- src/components/NavButton.js
+++
@@ -1,8 +0,0 @@
-export default function App() {
-    const navigate = useNavigate();
-    return (
-        <div className="App">
-            <button onClick={() => navigate("./components/AboutMe")}> About Me </button>
-        </div>
-    );
-}
